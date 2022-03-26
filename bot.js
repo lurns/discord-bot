53a6d101debefa870ebfe5175966ed69ef82043b
lurns <lrnlee08@gmail.com>
2022-03-26 16:17:23 -0500

    set up slash commands

--- bot.js
+++ bot.js
@@ -1,17 +1,43 @@
 import dotenv from 'dotenv';
-
 dotenv.config()
 
-import { Client, Intents } from 'discord.js';
+import { Client, Collection, Intents } from 'discord.js';
+import fs from 'node:fs'
 
 const client = new Client({ 
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILDS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS],
   partials: ['MESSAGE','REACTION'] 
 });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = await import (`./commands/${file}`);
+
+	client.commands.set(command.default.data.name, command);
+}
 
 client.on('ready', () => {
   console.log('bot has logged in')
   console.log(`${client.user.username}`)
+})
+
+client.on('interactionCreate', async (interaction) => {
+  console.log('uhhh')
+  if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+    console.log(command)
+		await command.default.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 })
 
 client.login(process.env.CRAB_BOT_TOKEN);
---
+++ deploy-commands.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+import fs from 'node:fs'
+import {REST} from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = await import (`./commands/${file}`);
+
+	commands.push(command.default.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.CRAB_BOT_TOKEN);
+
+await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+// (async () => {
+//   try {
+//     console.log('Started refreshing application (/) commands.');
+
+//     await rest.put(
+//       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+//       { body: commands },
+//     );
+
+//     console.log('Successfully reloaded application (/) commands.');
+//   } catch (error) {
+//     console.log('error')
+//     console.error(error);
+//   }
+// })();
