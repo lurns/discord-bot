191af6f62752b5024cb777da6963e67cc6a70e79
lurns <lrnlee08@gmail.com>
2022-04-17 13:30:29 -0500

    update puppeteer config for heroku

--- services/delicious-handler.js
+++ services/delicious-handler.js
@@ -12,7 +12,7 @@ export const fetchRecipe = async (interaction) => {
   }
 
   // set up puppeteer
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage()
   await page.goto(`https://damndelicious.net/?s=${searchTerm}`, {waitUntil: 'networkidle0'});
 
