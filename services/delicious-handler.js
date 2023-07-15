ea07f2e12231ce8794198844ae546396f3867b5b
Lauren Lee <lrnlee08@gmail.com>
2023-07-14 19:39:04 -0500

    add another website for recipes

--- services/delicious-handler.js
+++ services/delicious-handler.js
@@ -1,20 +1,10 @@
 import puppeteer from "puppeteer";
 
-export const fetchRecipe = async (interaction) => {
-  const keywords = ['italian', 'asian', 'noodle', 'rice', 'chicken', 'thai', 'healthy', 'easy', 'pasta']
-  let searchTerm = '';
-
-  // if interaction, get a search term from it if given
-   if (interaction && interaction.options.get("keyword")) {
-    searchTerm = interaction.options.get("keyword").value;
-  } else {
-    searchTerm = keywords[Math.floor(Math.random() * keywords.length)];
-  }
-
+const deliciousScrape = async (searchTerm) => {
   // set up puppeteer
-  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage()
-  await page.goto(`https://damndelicious.net/?s=${searchTerm}`, {waitUntil: 'domcontentloaded'});
+  await page.goto(`https://damndelicious.net/?s=${searchTerm}`, { waitUntil: 'domcontentloaded' });
 
   // get page numbers
   let pageNum = 1;
@@ -32,14 +22,14 @@ export const fetchRecipe = async (interaction) => {
   });
 
   // search via page num
-  if (findPagination[findPagination.length-1] > 1) {
-    pageNum = Math.floor(Math.random() * findPagination[findPagination.length-1]);
+  if (findPagination[findPagination.length - 1] > 1) {
+    pageNum = Math.floor(Math.random() * findPagination[findPagination.length - 1]);
 
     if (pageNum < 1) {
       pageNum = 1;
     }
-    
-    await page.goto(`https://damndelicious.net/page/${pageNum}/?s=${searchTerm}`, {waitUntil: 'domcontentloaded'});
+
+    await page.goto(`https://damndelicious.net/page/${pageNum}/?s=${searchTerm}`, { waitUntil: 'domcontentloaded' });
   }
 
   // choose a random recipe on the page
@@ -50,7 +40,7 @@ export const fetchRecipe = async (interaction) => {
 
     // parse recipe
     const recipeChoice = recipeElements[recipeNum];
-    
+
     let recipeURL = recipeChoice.querySelector('a');
     recipeURL = recipeURL.getAttribute('href');
 
@@ -67,7 +57,75 @@ export const fetchRecipe = async (interaction) => {
 
   });
 
-  await browser.close()
+  await browser.close();
+
+  return recipe;
+}
+
+const harvestScrape = async (searchTerm) => {
+  // set up puppeteer
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+  const page = await browser.newPage()
+  await page.goto(`https://www.halfbakedharvest.com/#search/q=${searchTerm}`, { waitUntil: 'domcontentloaded' });
+
+  // get results
+  const frames = await page.frames();
+  const topResultsButton = await frames[0].waitForSelector('pierce/#titleLink');
+  await topResultsButton.click();
+
+  await page.waitForSelector('pierce/#slickTemplateCellContainer');
+
+  // get recipes
+  const recipeElements = await page.$$eval(
+    "pierce/#slickTemplateCellContainer",
+    r => r.map(e => e)
+  );
+
+  await browser.close();
+
+  // select + format a random recipe
+  const recipeNum = Math.floor(Math.random() * recipeElements.length);
+  const recipeChoice = recipeElements[recipeNum].slickPageDescriptor;
+
+  let recipeURL = recipeChoice.url;
+  let recipeIMG = recipeChoice.image.url;
+  let recipeTitle = recipeChoice.titleText;
+  // TODO: description might have some markup that i'll have to
+  // snip out to render more nicely on discord
+  let recipeDesc = recipeChoice.descriptionHtml;
+
+  console.log(recipeURL, recipeIMG, recipeTitle, recipeDesc);
+
+  return { recipeURL, recipeIMG, recipeTitle, recipeDesc };
+}
+
+export const fetchRecipe = async (interaction) => {
+  const keywords = [
+    'italian', 'asian', 'noodle', 'rice', 'chicken', 
+    'thai', 'healthy', 'easy', 'pasta', 'ground beef', 
+    'garlic', 'burger'
+  ];
+  let searchTerm = '';
+
+  // if interaction, get a search term from it if given
+  if (interaction && interaction.options.get("keyword")) {
+    searchTerm = interaction.options.get("keyword").value;
+  } else {
+    searchTerm = keywords[Math.floor(Math.random() * keywords.length)];
+  }
+
+  // pick a site to get a recipe
+  const site = Math.round(Math.random());
+  let recipe;
+
+  switch (site) {
+    case 0:
+      recipe = await deliciousScrape(searchTerm);
+    case 1:
+      recipe = await harvestScrape(searchTerm);
+    default:
+      recipe = await deliciousScrape(searchTerm);
+  }
 
   // format message embed
   const recipeEmbed = {
@@ -82,3 +140,5 @@ export const fetchRecipe = async (interaction) => {
 
   return recipeEmbed;
 }
+
+await harvestScrape();
